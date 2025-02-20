dfebcaf86cca1e6350410d60c5156b0ba4f74f74
amiraulc <amiraulc@union.edu>
2025-02-19 23:21:28 -0500

    Update ignition.h

--- modules/ignition/ignition.h
+++ modules/ignition/ignition.h
@@ -4,6 +4,8 @@
 #ifndef _IGNITION_H_
 #define _IGNITION_H_
 
+#include "mbed.h"
+#include "arm_book_lib.h"
 //=====[Declaration of public defines]=========================================
 
 //=====[Declaration of public data types]======================================
@@ -12,7 +14,7 @@
 
 void inputsInit();
 void outputsInit();
-void ignitionCase();
+void ignitionUpdate();
 void driverIntroduction();
 void drivingState();
 void uartCommands(int cmd);
@@ -24,6 +26,18 @@ bool getPassengerSeatbelt();
 bool isignitionon();
 
 void userInterfaceUpdate();
+bool getEngineState();
+
+typedef enum {
+    BUTTON_OFF_0,
+    BUTTON_ON_1,
+    BUTTON_READY_STATE,
+    BUTTON_ON_2
+} ignitionState_t;
+
+extern ignitionState_t ignition_State;
+
+
 
 //=====[#include guards - end]=================================================
 
